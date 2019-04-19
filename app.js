b36b7c2697190c1a936c48fb4742a6f13dd790a0
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-19 14:49:05 +0530

    Now update amount is added in the database
    and the data is not lost if you quit the page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { verifyLoginCredentials } = require("./loginHandler");
+const { updateAmountToDB } = require("./transactions");
 
 const PORT = process.env.PORT || 8080;
 const CLIENT_ADDRESS = "/e-wallet-simulation-client/build";
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(bodyParser.text());
 app.post("/loginCredentials", verifyLoginCredentials);
+app.post("/updateAmount", updateAmountToDB);
 app.use(express.static(__dirname + CLIENT_ADDRESS));
 
 app.listen(PORT, () => `server is listening at port ${PORT}`);
---
+++ transactions.js
@@ -0,0 +1,19 @@
+const { CONNECTION } = require("./databaseConfig");
+
+const updateAmountToDB = function(req, res) {
+  const details = JSON.parse(req.body);
+
+  const queryMessage = `update users set amount=${
+    details.newAmount
+  } where username = "${details.username}";`;
+
+  CONNECTION.query(queryMessage, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(result);
+  });
+};
+
+module.exports = { updateAmountToDB };
