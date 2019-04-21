eabffe195891bf81fa3dcc9c554e2df6446600b7
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-21 12:27:33 +0530

    inserting duplicate usernames while signing up generates warning

--- signup.js
+++ signup.js
@@ -2,16 +2,32 @@ const { CONNECTION } = require("./databaseConfig");
 
 const addSignUpCredentials = function(req, res) {
   const { username, password, name } = JSON.parse(req.body);
-  console.log(JSON.parse(req.body), "------");
-  const querryMessage =
+
+  const checkUsername = `select username from users where username = "${username}"`;
+
+  const insertCredentials =
     "insert into users (username, password, name, amount)" +
     `values("${username}", "${password}", "${name}", 0)`;
-  CONNECTION.query(querryMessage, (err, result) => {
+
+  CONNECTION.query(checkUsername, (err, result) => {
     if (err) {
-      console.error("error is ---- \n", err);
+      console.error("error while checking username ---- \n", err);
       return;
     }
-    console.log(result, "*****");
+
+    if (result.length) {
+      res.json({ duplicateUsername: true });
+      return;
+    }
+
+    CONNECTION.query(insertCredentials, (err, result) => {
+      if (err) {
+        console.error("error while inserting --- \n", err);
+        return;
+      }
+      console.log(result);
+      res.json({ duplicateUsername: false });
+    });
   });
 };
 
