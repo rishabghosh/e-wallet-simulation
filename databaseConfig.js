4d411cb73b9cae8b3f13b1fe6d1a2e207f6edda3
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-21 22:41:19 +0530

    use environment variables for database config

--- databaseConfig.js
+++ databaseConfig.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const CONNECTION = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "xiswalder",
-  database: "e_wallet"
+  host: process.env.HOST, //loacahost
+  user: process.env.USER, //root
+  password: process.env.PASSWORD, //xiswalder
+  database: process.env.DATABASE_NAME //e_wallet
 });
 
 module.exports = { CONNECTION };
